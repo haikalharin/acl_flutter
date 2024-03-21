5f9a3468fa21ecca7a02566e3083c0cd46b1c345
MuhammadHaikalHarin <v-m.haikal.harin@bukittechnology.com>
2024-03-21 15:07:47 +0700

    update faa private data and add flutter_helper

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -4,8 +4,8 @@ class ApiConfig {
   static const token =
       "6369de3d8da8e764cc3c1cbe4121bb6741875de2c30dfd39df8fd351b6a508ea";
   static const String baseUrl = "https://igbeyewo.allianz.co.id/acl/app";
-  static const Duration receiveTimeout = Duration(milliseconds: 15000);
-  static const Duration connectionTimeout = Duration(milliseconds: 15000);
+  static const Duration receiveTimeout = Duration(milliseconds: 60000);
+  static const Duration connectionTimeout = Duration(milliseconds: 60000);
   static const String users = '/users';
   static const String posts = '/posts';
   static const String comments = '/comments';
